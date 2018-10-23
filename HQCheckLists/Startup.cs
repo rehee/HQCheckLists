1c2f905c5f93113be427dd2436b8e12ff21e682c
Xudong Hao <Xudong.Hao@deltafs.co.uk>
2018-10-23 17:07:30 +0100

    try reference view

--- HQCheckLists/Startup.cs
+++ HQCheckLists/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using HQCheckLists.Models.Users;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
@@ -20,13 +22,22 @@ namespace HQCheckLists
       Configuration = configuration;
       HostingEnvironment = hostingEnvironment;
     }
-
+    //http://jackhiston.com/2017/8/15/sharing-controllers-and-views-in-aspnet-core/
     public IConfiguration Configuration { get; }
     public IHostingEnvironment HostingEnvironment { get; }
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      var assemblyView = Assembly.Load("SDHCC.Admins.Views");
+      services.AddMvc()
+        .AddApplicationPart(assemblyView);
+      services.Configure<RazorViewEngineOptions>(options =>
+      {
+        options.FileProviders.Add(
+            new EmbeddedFileProvider(assemblyView));
+      });
+
       StartUpFunction.ConfigureServices<HQUser, HQCheckLists.Models.Contents.ContentBaseModel, HQCheckLists.Models.DropDowns.DropDownModel>(
         services, Configuration, HostingEnvironment);
     }
@@ -41,6 +52,7 @@ namespace HQCheckLists
         RequestPath = "",
         EnableDirectoryBrowsing = false
       });
+
       StartUpFunction.Configure(app, env);
     }
   }
