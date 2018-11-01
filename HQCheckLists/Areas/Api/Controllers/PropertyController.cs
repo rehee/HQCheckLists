ddee2cb77afd3611e03ebc33c5e8b35e01734190
Xudong Hao <Xudong.Hao@deltafs.co.uk>
2018-11-01 08:41:52 +0000

    add api controller

---
+++ HQCheckLists/Areas/Api/Controllers/PropertyController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HQCheckLists.Models.Apis;
+using HQCheckLists.Models.Contents;
+using HQCheckLists.Services.PropertyService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HQCheckLists.Areas.Api.Controllers
+{
+  [Area("Api")]
+  public class PropertyController : Controller
+  {
+    IPropertyService property;
+    public PropertyController(IPropertyService property)
+    {
+      this.property = property;
+    }
+    public JsonResult Index()
+    {
+      var result = property.GetAllPropertyByUser(User, out var response);
+      return Json(new ApiResponse<IEnumerable<PropertyModel>>(response.Success, response.Message, result));
+    }
+    [HttpPost]
+    public JsonResult Create(PropertyModel model)
+    {
+      this.property.CreateProperty(model, User, out var response);
+      var result = new ApiResponse<PropertyModel>(response.Success, response.Message, model);
+      return Json(result);
+    }
+    [HttpPost]
+    public JsonResult Update(PropertyModel model)
+    {
+      this.property.CreateProperty(model, User, out var response);
+      var result = new ApiResponse<PropertyModel>(response.Success, response.Message, model);
+      return Json(result);
+    }
+  }
+}
---
+++ HQCheckLists/Areas/Api/Controllers/PropertyInventoryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HQCheckLists.Models.Contents;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HQCheckLists.Areas.Api.Controllers
+{
+  [Area("Api")]
+  public class PropertyInventoryController : Controller
+  {
+    public JsonResult Index()
+    {
+      return null;
+    }
+    [HttpPost]
+    public JsonResult GetPropertyInventory(IEnumerable<string>Ids)
+    {
+      return null;
+    }
+    [HttpPost]
+    public JsonResult CreatePropertyInventory(InventoryModel model)
+    {
+      return null;
+    }
+    [HttpPost]
+    public JsonResult UpdatePropertyInventory(InventoryModel model)
+    {
+      return null;
+    }
+    [HttpPost]
+    public JsonResult RemovePropertyInventory(string id)
+    {
+      return null;
+    }
+  }
+}
