5b12170c175c0194ab902a62f75f985b91bdc1cc
rehee <x3iii131@gmail.com>
2018-11-04 21:49:10 +0000

    完成了添加以及更新清洁计划

---
+++ HQCheckLists/Areas/Api/Controllers/CleaningItemController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HQCheckLists.Managers;
+using HQCheckLists.Models.Apis;
+using Microsoft.AspNetCore.Mvc;
+using SDHCC.DB.Content;
+
+namespace HQCheckLists.Areas.Api.Controllers
+{
+  [Area("Api")]
+  public class CleaningItemController : Controller
+  {
+    ICleaningItemManager cleaningItemManager;
+    public CleaningItemController(ICleaningItemManager cleaningItemManager)
+    {
+      this.cleaningItemManager = cleaningItemManager;
+    }
+    public JsonResult ReadPostModels(string cleaningId)
+    {
+      var result = cleaningItemManager.CleaningItemsPostByCleaningId(User, cleaningId);
+      return Json(new ApiResponse<IEnumerable<ContentPostModel>>(result != null, null, result));
+    }
+  }
+}
